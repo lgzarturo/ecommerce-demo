a982baf5561bc4457c0ba7429016742acb95272c
ArturoLG <lgzarturo@gmail.com>
2022-05-20 11:24:56 -0500

    paso6: dependencia con autowire

--- src/main/kotlin/com/revenatium/business/component/StartupEvent.kt
+++ src/main/kotlin/com/revenatium/business/component/StartupEvent.kt
@@ -3,13 +3,17 @@ package com.revenatium.business.component
 import com.revenatium.repository.ProductRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 
 @Component
-class StartupEvent(private val productRepository: ProductRepository) : ApplicationListener<ApplicationReadyEvent> {
+class StartupEvent : ApplicationListener<ApplicationReadyEvent> {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
 
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
         log.debug("Ejecutando evento al inicio de SpringBoot")
