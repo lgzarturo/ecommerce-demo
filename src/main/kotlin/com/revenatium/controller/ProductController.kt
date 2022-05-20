1ca07b4e4bf8bc4317d0a797f704b34d2fc0b005
ArturoLG <lgzarturo@gmail.com>
2022-05-20 16:41:29 -0500

    paso11: atrapando errores en el CRUD

--- src/main/kotlin/com/revenatium/controller/ProductController.kt
+++ src/main/kotlin/com/revenatium/controller/ProductController.kt
@@ -2,6 +2,8 @@ package com.revenatium.controller
 
 import com.revenatium.business.service.ProductService
 import com.revenatium.model.Product
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController
 class ProductController(
     private val productService: ProductService
 ) {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
     @GetMapping
     fun list() = productService.getProducts()
 
@@ -23,12 +27,19 @@ class ProductController(
     fun create(@RequestBody product: Product) = productService.addProduct(product)
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long) = productService.getProductById(id)
+    fun getById(@PathVariable id: Long) = productService.getProductById(id).orElseThrow {
+        IllegalArgumentException("Producto con el ID $id, no existe")
+    }
 
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody product: Product) = productService.updateProduct(id, product)
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long) = productService.deleteProduct(id)
+    fun delete(@PathVariable id: Long) = try {
+        productService.deleteProduct(id)
+    } catch (e: IllegalArgumentException) {
+        log.error("Error al intentar borrar el producto con el ID $id, ya que no existe")
+        throw IllegalArgumentException("El producto con el ID $id no puede ser borrado")
+    }
 
 }
