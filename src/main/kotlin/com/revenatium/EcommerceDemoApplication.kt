8402dd6a2d4b5d5c5d35325884804a465107a1d3
ArturoLG <lgzarturo@gmail.com>
2022-05-20 10:15:20 -0500

    paso1: proyecto base

---
+++ src/main/kotlin/com/revenatium/EcommerceDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.revenatium
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/revenatium/EcommerceDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.revenatium
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
